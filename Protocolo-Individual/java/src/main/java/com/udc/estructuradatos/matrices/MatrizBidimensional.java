acef85d6a3463ba0951b88feff966c24d1a76477
YhonBarriosGandara <ybarriosg@unicartagena.edu.co>
2026-09-17 22:56:20 -0500

    feat(matrices): crear matriz bidimensional 3x3 y mostrar en formato tabla

---
+++ Protocolo-Individual/java/src/main/java/com/udc/estructuradatos/matrices/MatrizBidimensional.java
@@ -0,0 +1,24 @@
+package com.udc.estructuradatos.matrices;
+
+public class MatrizBidimensional {
+
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Matriz:");
+
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+    }
+}
